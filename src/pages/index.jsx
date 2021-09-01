2284fb611863f3b7622ae05ef376d0e55eaac020
MasakiMimura <mimu36projectd@gmail.com>
2021-09-01 16:55:20 +0900

    💄 max-widthを600pxに制限

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import styles from "src/styles/Home.module.css";
 import { Header } from "src/components/Header";
 
 const Index = (props) => {
 	return (
 		<div className={styles.container}>
+			<Head>
+				<title>Index Page</title>
+			</Head>
 			<Header />
 			<h1>Next.jsで学ぶReact講座</h1>
 			<p>JSONPlaceholderのAPIを色々叩いてみるよ！</p>

5a26601127afa485b18d9d7263cbc2aac9242d98
MasakiMimura <mimu36projectd@gmail.com>
2021-09-01 17:33:54 +0900

    🚚 投稿系ページの名前変更

--- src/pages/posts/[id]/index.jsx
+++ src/pages/posts/[id]/index.jsx
@@ -1,9 +1,8 @@
-import Head from "next/head";
 import styles from "src/styles/Home.module.css";
 import { Header } from "src/components/Header";
 import { Post } from "src/components/Post";
 
-const PostId = () => {
+const PostsId = () => {
 	return (
 		<div className={styles.container}>
 			<Header />
@@ -12,4 +11,4 @@ const PostId = () => {
 	);
 };
 
-export default PostId;
+export default PostsId;

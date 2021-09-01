b954ba7528b33c5254f18ba161af5dee4403a251
MasakiMimura <mimu36projectd@gmail.com>
2021-09-02 07:34:42 +0900

    🎨 Layoutをコンポーネントを使ってまとめた

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,16 @@
 import Head from "next/head";
-import "src/styles/globals.css";
+import { Layout } from "src/components/Layout";
 
 const MyApp = ({ Component, pageProps }) => {
 	return (
-		<>
+		<div>
 			<Head>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<Component {...pageProps} />;
-		</>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</div>
 	);
 };
 

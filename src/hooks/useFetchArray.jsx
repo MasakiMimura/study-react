3a72532b1976bf9202638c4219b5943eb1c717ca
MasakiMimura <mimu36projectd@gmail.com>
2021-09-14 08:02:17 +0900

    🚚 英語の間違いを修正。複数形になっていたものを単数形に変更。

--- src/hooks/useFetchArray.jsx
+++ src/hooks/useFetchArray.jsx
@@ -14,22 +14,25 @@ const useFetchArray = (url, fetcher) => {
 
 const API_URL = "https://jsonplaceholder.typicode.com";
 
-export const useComments = () => {
-	return useFetchArray(`${API_URL}/comments`, fetcher);
-};
-
+// posts
 export const usePosts = () => {
 	return useFetchArray(`${API_URL}/posts`, fetcher);
 };
 
+export const usePostsByUserId = (id) => {
+	return useFetchArray(id ? `${API_URL}/posts?userId=${id}` : null, fetcher);
+};
+
+// users
 export const useUsers = () => {
 	return useFetchArray(`${API_URL}/users`, fetcher);
 };
 
-export const useCommentsByPostsId = (id) => {
-	return useFetchArray(id ? `${API_URL}/comments?postId=${id}` : null, fetcher);
+// comments
+export const useComments = () => {
+	return useFetchArray(`${API_URL}/comments`, fetcher);
 };
 
-export const usePostsByUserId = (id) => {
-	return useFetchArray(id ? `${API_URL}/posts?userId=${id}` : null, fetcher);
+export const useCommentsByPostId = (id) => {
+	return useFetchArray(id ? `${API_URL}/comments?postId=${id}` : null, fetcher);
 };

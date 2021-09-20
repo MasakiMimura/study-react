c0c065c27e6c001e6ef5b3d2a7bc7c7886c57ac5
MasakiMimura <mimu36projectd@gmail.com>
2021-09-21 08:11:00 +0900

    ⚡️ 同一ページで複数APIがある場合のSSR

--- src/pages/users/[id]/index.jsx
+++ src/pages/users/[id]/index.jsx
@@ -4,13 +4,18 @@ import { SWRConfig } from "swr";
 
 export const getServerSideProps = async (ctx) => {
 	const { id } = ctx.query;
-	const API_URL = `https://jsonplaceholder.typicode.com/users/${id}`;
-	const user = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+	// ユーザー情報の取得
+	const USER_API_URL = `https://jsonplaceholder.typicode.com/users/${id}`;
+	const user = await fetch(USER_API_URL);
 	const userData = await user.json();
+	// ユーザーの投稿の取得
+	const POSTS_API_URL = `https://jsonplaceholder.typicode.com/posts?userId=${userData.id}`;
+	const posts = await fetch(POSTS_API_URL);
+	const postsData = await posts.json();
 
 	return {
 		props: {
-			fallback: { [API_URL]: userData },
+			fallback: { [USER_API_URL]: userData, [POSTS_API_URL]: postsData },
 		},
 	};
 };

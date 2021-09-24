e6b621abd2e3e60a62f932be4032fbeb9ba47a95
MasakiMimura <mimu36projectd@gmail.com>
2021-09-24 23:22:10 +0900

    ⚡️ コメント一覧ページでSGを実践

--- src/pages/comments/index.jsx
+++ src/pages/comments/index.jsx
@@ -1,15 +1,35 @@
 import Head from "next/head";
 import { CommentComponent } from "src/components/Comments";
 import { Header } from "src/components/Header";
+import { SWRConfig } from "swr";
 
-const Comments = () => {
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const getStaticProps = async () => {
+	const COMMENTS_API_URL = `https://jsonplaceholder.typicode.com/comments`;
+	const comments = await fetch(COMMENTS_API_URL);
+	const commentsData = await comments.json();
+	await sleep(2000);
+
+	return {
+		props: {
+			fallback: { [COMMENTS_API_URL]: commentsData },
+		},
+	};
+};
+
+const Comments = (props) => {
+	const { fallback } = props;
+
 	return (
 		<div>
 			<Head>
 				<title>Comments Page</title>
 			</Head>
-			<Header />
-			<CommentComponent />
+			<SWRConfig value={{ fallback }}>
+				<Header />
+				<CommentComponent />
+			</SWRConfig>
 		</div>
 	);
 };
--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -3,10 +3,13 @@ import { UsersComponent } from "src/components/Users";
 import { Header } from "src/components/Header";
 import { SWRConfig } from "swr";
 
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 export const getServerSideProps = async () => {
 	const USERS_API_URL = `https://jsonplaceholder.typicode.com/users`;
 	const users = await fetch(USERS_API_URL);
 	const usersData = await users.json();
+	await sleep(2000);
 
 	return {
 		props: {

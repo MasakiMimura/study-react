9542a08682e57870a4ee36e8077c1b58a4ffa32a
MasakiMimura <mimu36projectd@gmail.com>
2021-10-04 00:53:02 +0900

    🚨 不正な波括弧の削除

--- src/components/Comments/CommentsByPostId.jsx
+++ src/components/Comments/CommentsByPostId.jsx
@@ -22,7 +22,7 @@ export const CommentsByPostsId = (props) => {
 				return (
 					<li key={comment.id} className="border-b pb-2">
 						<Link href={`/comments/${comment.id}`}>
-							<a className="block hover:text-blue-500">{`${comment.body}}`}</a>
+							<a className="block hover:text-blue-500">{`${comment.body}`}</a>
 						</Link>
 					</li>
 				);
